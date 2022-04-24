3759593ae346b7b0acf524328affe56aee3b0ac0
poohcom1 <pooh677@gmail.com>
2022-04-25 00:26:54 +0700

    Removed sentry

--- src/main/components/App/App.tsx
+++ src/main/components/App/App.tsx
@@ -1,5 +1,5 @@
-import * as Sentry from "@sentry/react"
-import { Integrations } from "@sentry/tracing"
+// import * as Sentry from "@sentry/react"
+// import { Integrations } from "@sentry/tracing"
 import React from "react"
 import { defaultTheme } from "../../../common/theme/Theme"
 import MLRootStore from "../../../ml-analyzer/stores/MLRootStore" // @signal-ml
@@ -11,13 +11,13 @@ import { EmotionThemeProvider } from "../Theme/EmotionThemeProvider"
 import { GlobalCSS } from "../Theme/GlobalCSS"
 import { MuiThemeProvider } from "../Theme/MuiThemeProvider"
 
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  release: process.env.VERCEL_GIT_COMMIT_SHA,
-  environment: process.env.VERCEL_ENV,
-  integrations: [new Integrations.BrowserTracing()],
-  tracesSampleRate: 1.0,
-})
+// Sentry.init({
+//   dsn: process.env.SENTRY_DSN,
+//   release: process.env.VERCEL_GIT_COMMIT_SHA,
+//   environment: process.env.VERCEL_ENV,
+//   integrations: [new Integrations.BrowserTracing()],
+//   tracesSampleRate: 1.0,
+// })
 
 export function App() {
   return (
