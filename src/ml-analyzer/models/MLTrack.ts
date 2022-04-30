82722b4874a15fc4c73fee07e0dd0cce4c49abf5
poohcom1 <pooh677@gmail.com>
2022-04-30 20:03:23 +0700

    Added force monophonic and fix remove bug  -  Refactored disposer into mltrack

--- src/ml-analyzer/models/MLTrack.ts
+++ src/ml-analyzer/models/MLTrack.ts
@@ -1,21 +1,113 @@
-import { IReactionDisposer, makeObservable, observable } from "mobx"
+import _ from "lodash"
+import { IReactionDisposer, makeObservable, observable, reaction } from "mobx"
+import { isNoteEvent, NoteEvent } from "../../common/track"
+import { isLyricsEvent } from "../actions/lyrics"
 import MLRootStore from "../stores/MLRootStore"
-import Chunk from "./Chunk"
+import Chunk, { FetchState } from "./Chunk"
 
 export default class MLTrack {
   public disposer: IReactionDisposer
   public chunks: Chunk[] = []
+
+  public noteCache: NoteEvent[] = []
 
   // ML Data
   public model: string = ""
   public modelManifest: Record<string, any> = {}
   public modelOptions: Record<string, string | boolean | number> = {}
 
-  public readonly trackId
+  public trackId
 
-  constructor(disposer: IReactionDisposer, trackId: number) {
-    this.disposer = disposer
+  constructor(rootStore: MLRootStore, trackId: number, delayMS = 2000) {
     this.trackId = trackId
+
+    this.disposer = reaction(
+      // Track events
+      () => {
+        if (this.trackId < rootStore.song.tracks.length)
+          return rootStore.song.tracks[this.trackId]?.events
+        else return null
+      },
+      // Sideeffect: Generate new chunks on new note
+      // Could be better optimized if changed note is known
+      (events) => {
+        if (!events) {
+          return
+        }
+
+        // Get track wrapper
+        const mlTrack = rootStore.mlTrackStore.get(this.trackId)!
+
+        const noteEvents = events.filter(isNoteEvent)
+
+        const newEvents = noteEvents.filter(
+          (e1) => !mlTrack.noteCache.find((e2) => _.isEqual(e1, e2))
+        )
+
+        const eventsToDelete: number[] = []
+
+        for (const event of newEvents) {
+          const noteStart = event.tick
+          const noteEnd = event.tick + event.duration
+
+          for (const other of noteEvents) {
+            if (other.id === event.id) continue
+
+            const otherStart = other.tick
+            const otherEnd = other.tick + other.duration
+
+            if (noteStart > otherStart && noteStart < otherEnd) {
+              other.duration = noteStart - otherStart
+            } else if (noteEnd > otherStart && noteEnd < otherEnd) {
+              other.tick = noteEnd
+              other.duration = otherEnd - noteEnd
+            }
+
+            if (other.duration <= 0) {
+              eventsToDelete.push(other.id)
+            }
+          }
+        }
+
+        _.remove(events, (e) => eventsToDelete.includes(e.id))
+
+        // Generate new chunks
+        const noteEventsList = Chunk.splitNotes(events)
+        const metaEvents = []
+        const newChunks = []
+
+        if (mlTrack.hasMidiParam("lyrics"))
+          metaEvents.push(...events.filter(isLyricsEvent))
+
+        for (const noteEvents of noteEventsList) {
+          newChunks.push(new Chunk(noteEvents, metaEvents, mlTrack))
+        }
+
+        // Compare and replace old chunks
+        if (mlTrack) {
+          mlTrack.chunks = Chunk.replaceChunks(mlTrack.chunks, newChunks)
+
+          for (const chunk of mlTrack.chunks) {
+            if (
+              chunk.state == FetchState.UnFetched ||
+              chunk.state == FetchState.NeedData
+            ) {
+              chunk.delayedConvert(
+                rootStore,
+                (_state: FetchState) => {
+                  rootStore.mlTrackStore.triggerChange()
+                },
+                delayMS
+              )
+            }
+          }
+        }
+
+        mlTrack.noteCache = events.filter(isNoteEvent)
+      }
+    )
+
+    this.disposer.bind(this)
 
     makeObservable(this, {
       chunks: observable.deep,
--- src/ml-analyzer/stores/MLTracksStore.ts
+++ src/ml-analyzer/stores/MLTracksStore.ts
@@ -1,74 +1,8 @@
 import { pullAt } from "lodash"
-import { makeObservable, observable, reaction } from "mobx"
-import { isLyricsEvent } from "../actions/lyrics"
-import Chunk, { FetchState } from "../models/Chunk"
+import { makeObservable, observable } from "mobx"
+import Chunk from "../models/Chunk"
 import MLTrack from "../models/MLTrack"
 import MLRootStore from "./MLRootStore"
-
-/**
- * Generate track wrapper
- * @param id Track id
- * @param delayMS Delay in ms before analyzer
- * @returns
- */
-function createTrackAnalyzer(
-  rootStore: MLRootStore,
-  id: number,
-  delayMS = 2000
-): MLTrack {
-  const disposer = reaction(
-    // Track events
-    () => {
-      if (id < rootStore.song.tracks.length)
-        return rootStore.song.tracks[id]?.events
-      else return null
-    },
-    // Sideeffect: Generate new chunks on new note
-    // Could be better optimized if changed note is known
-    (events) => {
-      if (!events) {
-        return
-      }
-
-      // Get track wrapper
-      const mlTrack = rootStore.mlTrackStore.get(id)!
-
-      // Generate new chunks
-      const noteEventsList = Chunk.splitNotes(events)
-      const metaEvents = []
-      const newChunks = []
-
-      if (mlTrack.hasMidiParam("lyrics"))
-        metaEvents.push(...events.filter(isLyricsEvent))
-
-      for (const noteEvents of noteEventsList) {
-        newChunks.push(new Chunk(noteEvents, metaEvents, mlTrack))
-      }
-
-      // Compare and replace old chunks
-      if (mlTrack) {
-        mlTrack.chunks = Chunk.replaceChunks(mlTrack.chunks, newChunks)
-
-        for (const chunk of mlTrack.chunks) {
-          if (
-            chunk.state == FetchState.UnFetched ||
-            chunk.state == FetchState.NeedData
-          ) {
-            chunk.delayedConvert(
-              rootStore,
-              (_state: FetchState) => {
-                rootStore.mlTrackStore.triggerChange()
-              },
-              delayMS
-            )
-          }
-        }
-      }
-    }
-  )
-
-  return new MLTrack(disposer, id)
-}
 
 export default class MLTracksStore {
   public mlTracks: Array<MLTrack | undefined> = [undefined] // undefined tracks represent regular tracks
@@ -86,7 +20,7 @@ export default class MLTracksStore {
   }
 
   addTrack(rootStore: MLRootStore, trackId: number) {
-    const track = createTrackAnalyzer(rootStore, trackId)
+    const track = new MLTrack(rootStore, trackId)
 
     this.mlTracks.push(track)
 
@@ -98,7 +32,7 @@ export default class MLTracksStore {
   }
 
   insertTrack(rootStore: MLRootStore, trackId: number) {
-    const newTrack = createTrackAnalyzer(rootStore, trackId)
+    const newTrack = new MLTrack(rootStore, trackId)
 
     this.mlTracks.splice(trackId, 0, newTrack)
 
@@ -106,7 +40,11 @@ export default class MLTracksStore {
   }
 
   removeTrack(trackId: number) {
-    const wrapper = pullAt(this.mlTracks, trackId)[0]
+    for (let i = trackId; i < this.mlTracks.length; i++) {
+      this.mlTracks[i]!.trackId--
+    }
+
+    const wrapper = pullAt(this.mlTracks, [trackId])[0]
 
     wrapper?.disposer()
   }
@@ -120,6 +58,10 @@ export default class MLTracksStore {
   }
 
   delete(id: number) {
+    for (let i = id; i < this.mlTracks.length; i++) {
+      this.mlTracks[i]!.trackId--
+    }
+
     const wrapper = pullAt(this.mlTracks, id)[0]
 
     wrapper?.disposer()
